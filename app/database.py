e9ee028684d6329129fe7a5bc122b209169ed497
Seregi Péter <g9siy0@inf.elte.hu>
2025-03-14 22:58:45 +0100

    change database.py

--- app/database.py
+++ app/database.py
@@ -9,6 +9,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL.startswith("mysql://"):
+    DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
