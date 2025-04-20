8925fac77dc77018a954175bd7eb4a3134ec0d51
Seregi Péter <g9siy0@inf.elte.hu>
2025-04-20 11:40:04 +0200

    set min length for challenge description

--- app/schemas.py
+++ app/schemas.py
@@ -125,7 +125,7 @@ class MeasurementMethodEnum(str, Enum):
 
 class ChallengeBase(BaseModel):
     name: str = Field(..., min_length=1, max_length=100)
-    description: str = Field(..., max_length=500)  # ❗ Már nem Optional
+    description: str = Field(..., min_length=20, max_length=500)  # ❗ Már nem Optional
     count_reps: bool
     duration: Optional[int] = Field(None, ge=1, le=500)
     measurement_method: MeasurementMethodEnum
