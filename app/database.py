32375ec0c33759b05a243cdd15b51fbb7e13d993
Seregi Péter <g9siy0@inf.elte.hu>
2025-05-01 14:57:10 +0200

    code formatting

--- app/database.py
+++ app/database.py
@@ -16,6 +16,7 @@ engine = create_engine(DATABASE_URL, pool_recycle=1800, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 def get_db():
     db = SessionLocal()
     try:
